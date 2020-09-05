18823ef040e1d227d5c41cec0a229f70f5aba0f6
jack <jackburns0523@gmail.com>
2020-09-05 15:15:45 -0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+import requests
+import dateutil.parser
+import datetime
+def print_hi(name):
+    r = requests.get("https://api-v3.mbta.com/predictions?filter%5Bstop%5D=70066")
+    print r.json()["data"][0]["attributes"]["arrival_time"]
+    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+    yourdate = dateutil.parser.parse(r.json()["data"][0]["attributes"]["arrival_time"])
+    print(yourdate)
+    print(datetime.datetime.now())
+    print(datetime.datetime.now()-yourdate.replace(tzinfo=None))
+#subtract the current time from the arrival time
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# [1, 2, 3]

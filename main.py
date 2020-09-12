23bd410602c0ebfa3f7ec48db8c1030defbc8196
jack <jackburns0523@gmail.com>
2020-09-12 15:08:05 -0400

    show the time on the thing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import requests
 import dateutil.parser
 import datetime
+import os
 def print_hi(name):
     r = requests.get("https://api-v3.mbta.com/predictions?filter%5Bstop%5D=70066")
     print r.json()["data"][0]["attributes"]["arrival_time"]
@@ -10,7 +11,8 @@ def print_hi(name):
     yourdate = dateutil.parser.parse(r.json()["data"][0]["attributes"]["arrival_time"])
     print(yourdate)
     print(datetime.datetime.now())
-    print(yourdate.replace(tzinfo=None)-datetime.datetime.now().replace(tzinfo=None))
+    date = str(yourdate.replace(tzinfo=None)-datetime.datetime.now().replace(tzinfo=None))
+    os.system('sudo ../rpi-rgb-led-matrix/examples-api-use/scrolling-text-example -f "../rpi-rgb-led-matrix/fonts/7x13.bdf" --led-rows=32 --led-cols=64 --led-slowdown-gpio=3 -x 32 -y 9  -C 32,24,204 -B 255,247,3 "' + date + '"')
 #subtract the current time from the arrival time
 
 # Press the green button in the gutter to run the script.
